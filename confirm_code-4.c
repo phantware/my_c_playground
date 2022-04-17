0b282543da6621b9824687a3c45f692426ad66d6
phantware <phantmoney2011@gmail.com>
2022-04-17 12:06:51 +0000

    confirm_code-4 code wount run because one braces is missing

---
+++ confirm_code-4.c
@@ -0,0 +1,17 @@
+/*
+ * This is to test if the code will wrong
+ */
+
+#include <stdio.h>
+int main()
+{
+	 int card = 1;
+	  if (card > 1) {
+		   card = card - 1;
+		    if (card < 7)
+			     puts("Small card");
+	 else
+	     puts("Ace!");
+
+	   return 0;
+}
