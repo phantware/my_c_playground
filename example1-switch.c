bd0d60104542f9f1fc7e0c4d6e66e468b90076b6
phantware <phantmoney2011@gmail.com>
2022-04-17 15:03:15 +0000

    testing switch statement in C programming

---
+++ example1-switch.c
@@ -0,0 +1,28 @@
+/*
+ * Program to evaluate face value
+ * Release under the Vegas Public License.
+ * @ 2014 The College BlackJack Team
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	char card_name[3];
+	puts("Enter the card_name: ");
+	scanf("%2s", card_name);
+	int val = 0;
+switch(card_name[0]) {
+	case 'K':
+	case 'Q':
+	case 'J':
+		val = 10;
+		break;
+	case 'A':
+		val = 11;
+		break;
+	default:
+		val = atoi(card_name);
+	printf("The card value is: %i\n", val);
+}
+
+}
