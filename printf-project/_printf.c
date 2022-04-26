26470519f5db5bbd2ccef134854c66c119c34b76
Ismail Jamiu Babatunde <phantmoney2011@gmail.com>
2022-04-26 22:08:58 +0100

    task 0

---
+++ printf-project/_printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdarg.h>
+
+int _printf(const char * const format, ...)
+{
+ va_list args;
+ int i = 0;
+ int n_display = 0;
+ 
+ va_start(args, format);
+while (format[i] != '\0')
+{
+if (_putchar(format[i]))
+ n_display += 1;
+
+ i++;
+}
+
+
+var_end(args);
+}
