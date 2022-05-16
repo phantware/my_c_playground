a7296ce864413fb0679b514ae3fc1a8b5754b61d
phantware <phantmoney2011@gmail.com>
2022-05-16 16:15:34 +0000

    PID AND PPID Fully Explained

--- shell_stuff/main.c
+++ shell_stuff/main.c
@@ -6,7 +6,13 @@
 
 int main(int argc, char* argv[]){
 	int id = fork();
+	if(id ==0){
+		sleep(1);
+	}
 	printf("Current ID: %d, Parent ID: %d\n",
 			getpid(), getppid());
+	if(id != 0){
+		wait(NULL);
+	}
 	return 0;
 }
